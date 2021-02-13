d22f7be326eae273638a31bae5bb627ee5fa4f3d
Max <48172794+MaxGrishenko@users.noreply.github.com>
2021-02-13 16:30:04 +0300

    №3
    Входные данные - файл CSV. Выходные - SQL-запросы INSERT для переноса их в базу данных.
    Первая строка входного файла содержит имя таблицы БД, вторая - имена соотвествующих полей в этой таблице.

---
+++ lab3/output.sql
@@ -0,0 +1,6 @@
+INSERT INTO students(id, name, group_id) VALUES(1, Максим, 1);
+INSERT INTO students(id, name, group_id) VALUES(2, Андрей, 1);
+INSERT INTO students(id, name, group_id) VALUES(3, Александр, 2);
+INSERT INTO students(id, name, group_id) VALUES(4, Михаил, 1);
+INSERT INTO students(id, name, group_id) VALUES(5, Владислав, 2);
+INSERT INTO students(id, name, group_id) VALUES(6, Георгий, 1);
